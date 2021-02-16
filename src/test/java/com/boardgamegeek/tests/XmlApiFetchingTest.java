64e06eba10ef3c67e42269da00d631a9a6d87cae
Piotr_K <karasiukpiotr94@gmail.com>
2021-02-16 11:12:26 +0100

    "XmlApiFetchingTest"

--- src/test/java/com/boardgamegeek/tests/XmlApiFetchingTest.java
+++ src/test/java/com/boardgamegeek/tests/XmlApiFetchingTest.java
@@ -1,13 +1,43 @@
 package com.boardgamegeek.tests;
 
 import com.boardgamegeek.utilities.Log;
+import io.restassured.path.xml.XmlPath;
 import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasValue;
+import static org.hamcrest.Matchers.equalTo;
 
 public class XmlApiFetchingTest extends BaseTest {
+
+    @Test
+    public void fetchingXmlFileTest() {
+        signInFragment.clickSignInButton()
+                .signIn(loginProperties.getUsername(), loginProperties.getPassword());
+        assertThat(userMenuFragment.getUserName()).isEqualTo(loginProperties.getUsername());
+        Log.logInfo("Username: " + loginProperties.getUsername()
+                + ", Password:  " + loginProperties.getPassword());
+        menuFragment.goToAllBoardgames();
+        allBoardgamesPage.goToRandomGame();
+        Log.logInfo(driver.getCurrentUrl());
+        String gameId = driver.getCurrentUrl().replaceAll("\\D+", "");
+        Log.logInfo(gameId);
+        String languageDependence = gamePage.getLanguageDependenceInformation();
+        Log.logInfo(languageDependence + "________" + gamePage.getGameTitle());
+        XmlPath xmlFile = given().get("https://www.boardgamegeek.com/xmlapi/boardgame/" + gameId).xmlPath();
+        List<String> numVotesString = xmlFile.
+                get("boardgames.boardgame.poll.find{it.@name=='language_dependence'}.results.result.@numvotes");
+        int maxValue = Collections.max(numVotesString.stream().map(Integer::parseInt).collect(Collectors.toList()));
+        Log.logInfo(String.valueOf(maxValue));
+        String dependenceFromXml = xmlFile.
+                get("boardgames.boardgame.poll.find{it.@name=='language_dependence'}.results.result.find{it.@numvotes=='" + maxValue + "'}.@value");
+        Log.logInfo(dependenceFromXml);
+        assertThat(dependenceFromXml).isEqualTo(languageDependence);
+    }
 
     @Test
     public void fetchingRequestTest() {
@@ -18,9 +48,21 @@ public class XmlApiFetchingTest extends BaseTest {
                 + ", Password:  " + loginProperties.getPassword());
         menuFragment.goToAllBoardgames();
         allBoardgamesPage.goToRandomGame();
+        Log.logInfo(driver.getCurrentUrl());
+        String gameId = driver.getCurrentUrl().replaceAll("\\D+", "");
+        Log.logInfo(gameId);
         String languageDependence = gamePage.getLanguageDependenceInformation();
         Log.logInfo(languageDependence + "________" + gamePage.getGameTitle());
-        given().when().then().body("poll.results.result.value", hasValue(languageDependence));
-        Log.logInfo(given().when().then().body("poll.results.result", hasValue(languageDependence)).toString());
+        XmlPath xmlFile = given().get("https://www.boardgamegeek.com/xmlapi/boardgame/" + gameId).xmlPath();
+        List<String> numVotesString = xmlFile.
+                get("boardgames.boardgame.poll.find{it.@name=='language_dependence'}.results.result.@numvotes");
+        int maxValue = Collections.max(numVotesString.stream().map(Integer::parseInt).collect(Collectors.toList()));
+        Log.logInfo(String.valueOf(maxValue));
+        given().
+                when().
+                get("https://www.boardgamegeek.com/xmlapi/boardgame/" + gameId).
+                then().
+                body("boardgames.boardgame.poll.find{it.@name=='language_dependence'}.results.result.find{it.@numvotes=='" + maxValue + "'}.@value",
+                        equalTo(languageDependence));
     }
 }
